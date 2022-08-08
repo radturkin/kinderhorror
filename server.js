de7e935cc6b69297468e6f6ccb3a7c8f9e3212ea
JP Canindo <jp36tin@gmail.com>
2022-08-08 22:06:47 +0800

    added css file

--- server.js
+++ server.js
@@ -24,63 +24,76 @@ MongoClient.connect(process.env.connectionString, { useUnifiedTopology: true })
     //to read json
     app.use(bodyParser.json())
 //mdae change to line 31 {films: results}
-   
+
     app.get('/admin', (req, res) => {
-        db.collection('kinderhorror').find().toArray()
-          .then(results => {
-            res.render('pages/admin.ejs', {films: results})
+      res.render('pages/admin.ejs')
+      // db.collection('kinderhorror').find().toArray()
+      //   .then(results => {
+      //     res.render('pages/admin.ejs', {films: results})
+      //   })
+      //   .catch(error => console.error(error))
+    })
 
-          })
-          .catch(error => console.error(error))
-
+    app.get('/', (req, res) => {
+      db.collection('kinderhorror').find().toArray()
+        .then(results => {
+          res.render('pages/index.ejs', {films: results})
         })
-        app.get('/', (req, res) => {
-          db.collection('kinderhorror').find().toArray()
-
-          .then(results => {
-            res.render('pages/index.ejs', {films: results})
-                  })
-                  .catch(error => console.error(error))
-
-                })
+        .catch(error => console.error(error))
+    })
 
 // // // Root Route
-app.get("/:name", function (req, res, next) {
-  let movieTitle = req.params.name
-
-  db.collection("kinderhorror")
-    .find({ title: movieTitle })
-    .toArray()
-
-    .then(results => {
-    
-  
-    let movie = (results[0])
-    console.log(movie.trailer)
-    // res.json(movie)
-    // res.render('pages/api'
-    res.render('pages/api', {
-      title: movie.title,
-      review: movie.review,
-      summary: movie.summary,
-      kids: movie.forKids,
-      rating: movie.rating,
-      triggers: movie.triggers,
-      imdblink: movie.imdb,
-      wikilink: movie.wiki, 
-      trailer: movie.trailer,
+    app.get("/:name", function (req, res, next) {
+      let movieTitle = req.params.name
+      if (movieTitle === "random") {
+        db.collection("kinderhorror").find().toArray()
+          .then(results => {
+            const randomNum = Math.floor(Math.random() * results.length)
+            let movie = results[randomNum]
+            console.log("User wanted a random film title: " + movie.title)
+            res.render('pages/api', { movie: movie
+              // title: movie.title,
+              // review: movie.review,
+              // summary: movie.summary,
+              // kids: movie.forKids,
+              // rating: movie.rating,
+              // triggers: movie.triggers,
+              // imdblink: movie.imdb,
+              // wikilink: movie.wiki, 
+              // trailer: movie.trailer,
+            })
+          })
+          .catch(error => console.error(error))
+      }
+      db.collection("kinderhorror")
+      .find({ title: movieTitle })
+      .toArray()
+      .then(results => {
+        let movie = results[0]
+        console.log("User selected " + movie.title)
+        // res.json(movie)
+        // res.render('pages/api'
+        res.render('pages/api', { movie: movie
+          // title: movie.title,
+          // review: movie.review,
+          // summary: movie.summary,
+          // kids: movie.forKids,
+          // rating: movie.rating,
+          // triggers: movie.triggers,
+          // imdblink: movie.imdb,
+          // wikilink: movie.wiki, 
+          // trailer: movie.trailer,
+        })
+      })
+      .catch(error => console.error(error))
     })
-  })
-    .catch(error => console.error(error))
-
-  })
-
 
     app.post("/film", (req, res)=>{
         filmCollection.insertOne(req.body)
         .then(result => {
-            console.log(result)        })
-            res.redirect("/")
+          console.log(result)        
+          res.redirect("/")
+        })
         .catch(error => console.error(error))
     })
    
